02ab28375044f60ad4103cc9516c54ab5c52679e
selectormalik <malik.idbeis@gmail.com>
2015-04-02 13:15:11 -0500

    Ad Astra Entry
    entrance test for Ad Astra courses.

---
+++ entrance.rb
@@ -0,0 +1,67 @@
+#Global Variables
+$soap = 0
+$bread = 0
+$cereal = 0
+$dogFood = 0
+$total = 0
+$buy = Integer 
+
+#User Prompt
+def buyItem()
+	puts "1 - soap: $5"
+	puts "2 - bread: $2"
+	puts "3 - cereal: $3"
+	puts "4 - dog food: $5"
+	puts "0 - if you're done shopping"
+	print "Type the number of what you'd like to buy"
+		$buy = gets.chomp
+	#Entry Check
+	puts $buy
+	theMeat()
+end
+
+
+#The MEAT O' THE PROGRAM
+
+def theMeat()
+	if $buy == 1 
+	    $soap += 1
+	    $total += 5
+		puts "Soap X #{$soap}"
+		puts "Total $#{$total}"
+		puts "Thank you, continue!"
+		puts ""
+		buyItem()
+	elsif $buy == 2 
+	    $bread += 1
+	    $total += 2
+		puts "Bread X #{$bread}"
+		puts "Total $#{$total}"
+		puts "Thank you, continue!"
+		puts ""
+	elsif $buy == 3 
+	    $cereal += 1
+	    $total += 3
+		puts "Cereal X #{$cereal}"
+		puts "Total $#{$total}"
+		puts "Thank you, continue!"
+		puts ""
+	elsif $buy == 4 
+	    $dogFood += 1
+	    $total += 5
+		puts "Dog Food X #{$dogFood}"
+		puts "Total $#{$total}"
+		puts "Thank you, continue!"
+		puts ""
+	elsif $buy == 0
+		puts "Soap X #{$soap}" 
+		puts "Bread X #{$bread}"
+	   	puts "Cereal X #{$cereal}"
+		puts "Dog Food X #{$dogFood}"
+		puts "Total $#{$total}"
+		puts "Thank you for shopping with us!"
+		puts ""
+	end
+end
+
+buyItem()
